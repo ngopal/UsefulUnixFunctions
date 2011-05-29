17d82e36b11bd2710057aea43ca765b94fe63ff8
Nikhil Gopal <nikhilgopal@gmail.com>
2011-05-29 00:58:14 -0700

    fixed the median.sh script

--- bin/median.sh
+++ bin/median.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-sort | MEDROW=`awk 'END{print NR/2}'`
-echo $MEDROW
+sort -n | awk '{count[NR] = $0}END{if (NR%2){print count[int((NR+1)/2)]}else{print (count[int(NR / 2)] + count[int(NR / 2) + 1]) / 2.0}}'
